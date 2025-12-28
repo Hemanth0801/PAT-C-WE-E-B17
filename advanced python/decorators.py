157f694055c6bbe94452e2e77af13756e0f3176e
PAT-C-WE-E-B17 <nasinahemanth0801@gmail.com>
2025-12-28 21:45:28 +0530

    new and changed data

---
+++ advanced python/decorators.py
@@ -0,0 +1,2 @@
+#decoraters allows us to add functionality to an existing functionality
+#it wont change the actual code,just add functionality
--- advanced python/generators.py
+++ advanced python/generators.py
@@ -1,2 +1,6 @@
-#generators are function which rreturns a iterators(squence of items)
-#this function used  yield instand of return
+#generators are function which returns a iterators(squence of items)
+#this function used  yield insted of return
+# in function return statement , in generators yield statement
+
+#generator function contains yield statement -> which pauses the execution and returns the
+#iterator value and then resume the next step defined inside the generator function
