1087bd2f282cd4eaac8433c887dbbd548686fc29
PAT-C-WE-E-B17 <nasinahemanth0801@gmail.com>
2025-12-29 22:31:10 +0530

    new and changed data

--- PAT TASK-3/RANDOM NUMBERS.py
+++ PAT TASK-3/RANDOM NUMBERS.py
@@ -1,12 +1,12 @@
 import random
-num=random.randint(1, 10)
+num=random.randint(1, 10)  #randint is used to choose the random num
 print("Guess a number between 1 and 10")
-while True:
-    i = int(input("Enter your guess: "))
+while True:                            #loop
+    i = int(input("Enter your guess: "))  #input data we need to  enter
     if i < num:
-        print("Too low! Try again.")
+        print("Too low! Try again.")     #if i value is less than ramdom num
     elif i > num:
-        print("Too high! Try again.")
+        print("Too high! Try again.")     #f i value is greater than ramdom num
     else:
-        print("Correct! You guessed the number!")
+        print("Correct! You guessed the number!")     #else means thers is only option "="
         break
--- PAT TASK-3/Word scramble.py
+++ PAT TASK-3/Word scramble.py
@@ -1,12 +1,15 @@
 import random
-words = ['python', 'java', 'selenium', 'guvi']
-word = random.choice(words)
-scrambled = ''.join(random.sample(word, len(word)))
-print("Unscramble this word:", scrambled)
-guess = ""
-while guess != word:
-    guess = input("Enter your guess: ")
-    if guess == word:
-        print("Correct")
+words = ['python','javascript','java','automation','pytest','guvi','selenium']
+random_word = random.choice(words)  #it will choose random word from the list
+letters= list(random_word)
+random.shuffle(letters)     #shuffle the selected word
+scrambled_word = ''.join(letters)    #shuffled word get joined
+print("scrambled_word: ", scrambled_word)
+while True:
+    guess = input("Guess a letter: ")
+    if guess == random_word:     #if guess correct loop will break or else it will continue
+        print("Correct!")
+        break
     else:
-        print("Wrong guess. Try again")
+        print("Incorrect!, try again.")
+
