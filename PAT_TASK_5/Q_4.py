48c8badb96d79d64de687c2548ff4131a04b28c1
PAT-C-WE-E-B17 <nasinahemanth0801@gmail.com>
2026-01-01 22:30:52 +0530

    new and changed data

---
+++ PAT_TASK_5/Q_4.py
@@ -0,0 +1,5 @@
+#to check the string is number or not we use (.isdigit())
+
+num = lambda n: n.isdigit()  #it checkts the string num or not
+print(num("2"))              # true
+print(num("3a"))              #false
